14e5f400bbe2e25096aef4652b1f93b9e80238f3
pietropr <pintonpietro@gmail.com>
2018-08-24 11:59:02 -0300

    Update Method | OK

--- controller/AutorController.js
+++ controller/AutorController.js
@@ -5,9 +5,25 @@ var urlEncondedParse = bodyParser.urlencoded({extended:false});
 
 var Autor = require('../models/autorModel');
 
+//verifica login
+autorRoute.use(function (req, res, next) {
+    if(req.app.get('usuario').nome == '') {
+        res.redirect('/')
+    }
+    next();
+});
+
+
+autorRoute.get('/', function (req,res) {
+
+    Autor.find(null, null, {sort: {nome: 1}} , function(erro, autores) {
+        if(erro) return console.log(erro);
+        res.render('autor/autores', {autores: autores, usuario: req.app.get('usuario')})
+    });
+});
 
 autorRoute.get('/novo', function(req, res) {
-    res.render('autor/novo', {usuario: req.app.get('usuario')})
+    res.render('autor/novo', {autor: '', usuario: req.app.get('usuario')})
 });
 
 autorRoute.post('/novo', urlEncondedParse, function (req, res) {
@@ -21,6 +37,26 @@ autorRoute.post('/novo', urlEncondedParse, function (req, res) {
     autor.save(function (erro, autor) {
         if(erro) return console.erro(erro);
         res.render('autor/incluido', {usuario: req.app.get('usuario')});
+    })
+});
+
+autorRoute.get('/:id', function(req, res) {
+    Autor.findById(req.params.id, function (erro, autor) {
+        res.render('autor/autor', {autor: autor, usuario: req.app.get('usuario')});
+    });
+});
+
+autorRoute.post('/:id', urlEncondedParse, function(req, res) {
+    var autor = {
+       nome: req.body.nome,
+       email: req.body.email,
+       senha: req.body.senha,
+       admin: (req.body.admin == 'on')
+    };
+
+    Autor.findOneAndUpdate({_id: req.params.id}, autor, function (erro) {
+        if(erro) return console.log(erro);
+        res.render('autor/atualizado', {usuario: req.app.get('usuario')});
     })
 });
 
